f3e7538c797eda8ef071ff446dca8491ef8ebe59
Pavel Karpenko <ospkiev@gmail.com>
2021-07-11 18:07:08 +0300

    be6616f

---
+++ _nuxt/static/1626016015/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fcurrencies\u002F_nuxt\u002Fstatic\u002F1626016015",layout:"default",error:a,state:{currencies:{currencies:[]}},serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fcurrencies\u002F",assetsPath:"\u002Fcurrencies\u002F_nuxt\u002F",cdnURL:a}}}}(null));
